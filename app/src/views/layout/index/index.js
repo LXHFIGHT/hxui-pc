ad03197626e3dd7bd72b23e35077c76bfb63818a
lxhfight <lxhfight51@outlook.com>
2017-12-20 20:59:32 +0800

    优化移动端侧边栏效果，同时在移动端添加侧边栏展示隐藏按钮

--- app/src/views/layout/index/index.js
+++ app/src/views/layout/index/index.js
@@ -11,4 +11,28 @@ IndexCtrl.controller('IndexCtrl', ['$scope', '$rootScope', ($scope, $rootScope) 
     $scope.toggleSidebar = () => {
         $rootScope.showSidebar = ! $rootScope.showSidebar;
     };
+
+    $scope._init = function() {
+        window.addEventListener('click', function(e){
+            let $view = $(e.target);
+            let parents = $view.parents();
+            let isOnSidebar = false;
+            for (let i = 0; i < parents.length; i++) {
+                let item = parents[i].getAttribute('class') || '';
+                if (item.indexOf('hx-sidebar') !== -1 || item.indexOf('btn-show-sidebar') !== -1) {
+                    isOnSidebar = true;
+                    break;
+                }
+            }
+            let className = $view.attr('class') || '';
+            if (className.indexOf('hx-sidebar') !== -1 ||
+                className.indexOf('btn-show-sidebar') !== -1) {
+                isOnSidebar = true;
+            }
+            if (!isOnSidebar) {
+                $rootScope.showSidebar = false;
+                $rootScope.$apply();
+            }
+        });
+    }();
 }]);
